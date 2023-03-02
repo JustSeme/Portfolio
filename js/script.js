250db1de3c3fe67cf719454a1e55e65054087679
JustSemen <semyn03@mail.ru>
2023-03-02 18:03:28 +0300

    add integration with telegram sender bot

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function () {
-
 	jQuery('#send-form, #send-form-modal').click(function (e) {
 		e.preventDefault()
 		let form
@@ -10,22 +9,32 @@ jQuery(document).ready(function () {
 		}
 
 		if (form.valid()) {
-			//form.css('opacity', '.5');
-			var actUrl = form.attr('action');
+			form.css('opacity', '.5');
+			form.find('.site-btn').prop("disabled", true)
+			setTimeout(() => {
+				form.css('opacity', '1');
+				form.trigger('reset')
+				form.find('.request-message').html('');
+			}, 5000)
+
+			const currentValues = form.serializeArray()
+			const message = `Привет от ${currentValues[0].value}. Он написал: ${currentValues[2].value}. Свяжись с ним через: ${currentValues[1].value}`
+
+			const bot = '6098281339:AAGJ16BQ736dsxfg2889xql2492sVZJRZvE' //please dont still it
+			const chatId = '-918377010'
+			const actUrl = `https://api.telegram.org/bot${bot}/sendMessage?chat_id=${chatId}&text=${message}`
 
 			jQuery.ajax({
 				url: actUrl,
 				type: 'post',
 				dataType: 'html',
-				data: form.serialize(),
-				success: function (data) {
-					//form.html(data);
-					//form.css('opacity', '1');
-					form.find('.form-validation').html('форма отправлена успешно');
-					//$('#myModal').modal('show') // для бутстрапа
+				data: currentValues,
+				success: function () {
+					form.css('opacity', '1');
+					form.find('.request-message').html('форма отправлена успешно');
 				},
 				error: function () {
-					form.find('.status').html('серверная ошибка');
+					form.find('.request-message').html('Произошла какая-то ошибка... Попробуйте связаться со мной через социальные сети!');
 				}
 			});
 		}
